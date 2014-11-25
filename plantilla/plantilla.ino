929657ee6010d14749179558b2fb6183e1948adf
mp10arilacetania <mp10arilacetania@users.noreply.github.com>
2014-11-25 14:23:53 +0100

    Deixar plantilla buida

--- plantilla/plantilla.ino
+++ plantilla/plantilla.ino
@@ -1,6 +1,6 @@
 /**********************************************************************************
 **                                                                               **
-**                                  Morse                                        **
+**                               Plantilla                                       **
 **                                                                               **
 **                                                                               **
 **********************************************************************************/
@@ -8,122 +8,15 @@
 //********** Includes *************************************************************
 
 //********** Variables ************************************************************
-// pin for speaker
-const int pin = 8;
-
-int note = 1047;
-int velocitat = 10;
 
 //********** Setup ****************************************************************
 void setup()
 {
-
 }
 
 //********** Loop *****************************************************************
 void loop()
 {
-  /*
-  //S   ** Exemple SOS **
-  punt();
-  punt();
-  punt();
-  //Espai entre lletres
-  espaiL();
-  //O
-  ratlla();
-  ratlla();
-  ratlla();
-  //Espai entre lletres
-  espaiL();
-  //S
-  punt();
-  punt();
-  punt();
-  //Espai entre paraules
-  espaiP();
-  */
-  
-  // ** Exemple CURS A... **
-  ratlla();
-  punt();
-  ratlla();
-  punt();
-  
-  espaiL();
-  
-  punt();
-  punt();
-  ratlla();
-  
-  espaiL();
-  
-  punt();
-  ratlla();
-  punt();
-  
-  espaiL();
-  
-  punt();
-  punt();
-  punt();
-  
-  espaiP();
-  
-  punt();
-  ratlla();
-  
-  espaiL();
-  
-  punt();
-  ratlla();
-  punt();
-  ratlla();
-  punt();
-  ratlla();
-  
-  espaiL();
-  
-  punt();
-  ratlla();
-  punt();
-  ratlla();
-  punt();
-  ratlla();
-  
-  espaiL();
-  
-  punt();
-  ratlla();
-  punt();
-  ratlla();
-  punt();
-  ratlla(); 
-  
-  espaiP();
 }
 
 //********** Funcions *************************************************************
-void punt()
-{
-  tone(pin, note, 1000/velocitat);
-  delay(1000/velocitat); 
-  delay(1000/velocitat); 
-}
-
-void ratlla()
-{
-  tone(pin, note, 3*(1000/velocitat));
-  delay(3*(1000/velocitat)); 
-  delay(1000/velocitat);  
-}
-
-void espaiL()  //Espai entre lletres
-{  
-  delay(2*(1000/velocitat));  
-}
-
-void espaiP()  //Espai entre paraules
-{   
-  delay(6*(1000/velocitat));  
-}
